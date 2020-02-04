3916b05e32fa74d52b3fdf114582120a2e1f94c1
Craig Hughes <hghscraig@gmail.com>
2020-02-04 21:16:17 +0000

    Home page design implemented

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import SearchIcon from '../assets/icons/search.svg';
+
 class Home extends React.Component {
     constructor(props){
         super(props);
@@ -7,8 +9,20 @@ class Home extends React.Component {
 
     render(){
         return(
-            <div>
-                <h1>Hello</h1>
+            <div className="container" id="home">
+                <h1 className="title">testmy<span>.page</span></h1>
+                <p className="subtitle">Test the responsiveness of your site</p>
+
+                <div className="search-field">
+                    <input type="search" placeholder="enter your url here..."></input>
+                    <button><img src={SearchIcon}/></button>
+                </div>
+
+                <p className="credit">
+                    a sample project by
+                    <br/>
+                    <a href="https://github.com/craigmhughes">Craig Hughes</a>
+                </p>
             </div>
         );
     }
