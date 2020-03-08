cb59ba4d73c8fd4f7196d1dc0e319d7787e05d08
Craig Hughes <hghscraig@gmail.com>
2020-03-08 13:37:48 +0000

    Removed test iframe from App.js

--- src/App.js
+++ src/App.js
@@ -23,9 +23,6 @@ class App extends React.Component {
 
       this.useUrl = this.useUrl.bind(this);
       this.setDevices = this.setDevices.bind(this);
-
-      // Refs
-      this.testIframe = React.createRef();
   }
 
   /**
@@ -64,7 +61,6 @@ class App extends React.Component {
   render(){
     return (
       <Router className="App">
-        <iframe src={this.state.url} ref={this.testIframe} height={1} width={1}/>
         <AnimatePresence exitBeforeEnter>
           <Switch location={this.props.history} key={this.props.history}>
             <Route exact path='/' render={(props)=><Home history={props.history}/>} />
